2fe3688c33c74cc9534bd9c42f63fb2d70ce2162
Deepu-700 <deepu007bhardwaj@gmail.com>
2025-08-20 14:11:33 +0530

    Deepanshu

--- src/Abstract.java
+++ src/Abstract.java
@@ -2,15 +2,12 @@ abstract class Abstract{
     String name;
     int roll_no;
 
-    public Abstract(String name) {
+    public Abstract(String name, int roll_no) {
         this.name = name;
-    }
-
-    public Abstract(int roll_no) {
         this.roll_no = roll_no;
     }
 
-    abstract class attend_class();
-    abstract class give_exam();
+    abstract void attend_class();
+    abstract void give_exam();
 
 }
---
+++ src/Mymain1.java
@@ -0,0 +1,7 @@
+public class Mymain1 {
+    public static void main(String[] args) {
+        CollegeStudent cs = new CollegeStudent("Deepu",77);
+        cs.attend_class();
+        cs.give_exam();
+    }
+}
